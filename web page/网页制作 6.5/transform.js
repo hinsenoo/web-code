8be0bb62546a32e37a00e679ea3ceb6b59841261
Enoo <38681072+hinsenoo@users.noreply.github.com>
2018-11-26 14:25:46 +0800

    Add files via upload

---
+++ web page/网页制作 6.5/transform.js
@@ -0,0 +1,13 @@
+var mychar = document.getElementById("box");
+
+function change1(mychar) {
+    mychar.style.backgroundImage = "url(img/photo1.png)";
+}
+
+function change2(mychar) {
+    mychar.style.backgroundImage = "url(img/photo2.png)";
+}
+
+function change3(mychar) {
+    mychar.style.backgroundImage = "url(img/photo3.png)";
+}
